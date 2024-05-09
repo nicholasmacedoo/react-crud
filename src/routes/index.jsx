afd3f33e3261b0c66b9a387952927daf2b2a07cc
Nicholas Macedo <33987691+nicholasmacedoo@users.noreply.github.com>
2024-05-09 20:12:48 -0300

    rota de visualizar produto

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import { SignIn } from "../pages/login";
 import { Dashboard } from "../pages/dashboard";
 import { PrivatePageLayout } from "../template";
+import { VisualizarProduto } from "../pages/dashboard/visualizar-produto";
 
 export const routes = createBrowserRouter([
     {
@@ -15,6 +16,10 @@ export const routes = createBrowserRouter([
             {
                 path: '/dashboard',
                 element: <Dashboard />
+            },
+            {
+                path: '/dashboard/:id', 
+                element: <VisualizarProduto />
             }
         ]
     }
